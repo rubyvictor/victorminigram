8ad9b711be042ba425ff8ee9e6b5035b4e787141
Victor <victor@3dlantis.com>
2016-03-02 13:18:34 +0000

    Added country select

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
 module ApplicationHelper
-end
+
+
+#app/helpers/application_helper.rb		
+	def country_name(country_code)
+			country = ISO3166::Country[country_code]
+			if country
+				country.translations[I18n.locale.to_s]
+			else
+				country_code		
+			end	
+		end
+	end
