25e1448ae3bfb636c7ae09875291b33cff49ba07
Victor <victor@3dlantis.com>
2016-03-05 05:30:46 +0000

    add passwords

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,9 @@
 class User < ActiveRecord::Base
+	has_secure_password
 	validates_presence_of :name, :email
 	validates_format_of :email, with: /@/
+	validates_uniqueness_of :email, case_sensitive: false
+
+  before_save { self.email = email.downcase }
 
 end
