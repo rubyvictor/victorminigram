0627a3c6951883b214df62be1b8b29488cca2cbb
Victor <victor@3dlantis.com>
2016-03-05 06:23:54 +0000

    Sign in Sign out

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,10 @@ Rails.application.routes.draw do
   resources :grams
   resources :users
 
+  get    'sign_in'   => 'sessions#new'
+  post   'sign_in'   => 'sessions#create'
+  delete 'sign_out'  => 'sessions#destroy'
+
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
